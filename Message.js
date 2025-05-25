637fdd146b4c338b5023ca1b5e4b39326bbf5366
Andreehahay <144866456+Andreehahay@users.noreply.github.com>
2025-05-26 00:30:39 +0700

    Update Message.js

--- Message.js
+++ Message.js
@@ -5,7 +5,7 @@
   
   // Efek ketik per kata
   const message = `Hai sayang, selamat ulang tahun ya! <br>
-Hari ini spesial banget karena kamu lahir di dunia ini dan aku jadi punya kesempatan buat kenal kamu. Aku bersyukur banget punya kamu di hidupku. Kamu itu nggak cuma pacar, tapi juga sahabat terbaik aku. Ingat nggak dun pertama kali kita ketemueltah> <br>
+Hari ini spesial banget karena kamu lahir di dunia ini dan aku jadi punya kesempatan buat kenal kamu. Aku bersyukur banget punya kamu di hidupku. Kamu itu nggak cuma pacar, tapi juga sahabat terbaik aku. Ingat nggak dulu pertama kali kita ketemu<br>
 Aku nggak nyangka kalau orang yang awalnya cuma teman ngobrol ini bisa jadi segitu pentingnya buat aku. Kita udah ngelewatin banyak hal bareng, baik yang seneng-seneng sampe yang bikin stres. Tapi, tiap momen sama kamu selal bikin hidup aku lebih berwarna. Kamu selalu tau gimana caranya bikin aku ketawa, bahkan di hari-hari terburuk sekalipun. Semoga di ulang tahun kamu yang sekarang, semua harapan dan impian kamu bisa mulai tercapai satu per satu. Aku bakal selalu ada buat dukung kamu, apapun yang kamu pengen lakukan. Mari kita bikin lebih banyak kenangan manis dan seru di tahun-tahun ke depan. Sekali lagi, selamat ulang tahun ya, Sayang. <br>
 Semoga hari ini penuh dengan kebahagiaan dan cinta. Aku sayang kamu banget!💓💓`;
   
